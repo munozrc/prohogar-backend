328316d00aec35106d352136b9cf9a5f98bb9777
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-20 16:53:35 -0500

    fixed add category only pro

--- src/utils/createUser.ts
+++ src/utils/createUser.ts
@@ -1,4 +1,5 @@
 import { users } from "../models/Users";
+import { professionals } from "../models/Professionals";
 import { v4 as uuidv4 } from "uuid";
 import { UserModel } from "../typings";
 
@@ -10,10 +11,12 @@ export default function createUser(user: UserModel): UserModel {
     password: user.password,
     photo: user.photo || "",
     role: user.role || "",
-    category: user?.category,
   };
+  const category = user?.category;
 
   users.push(newUser);
+  if (typeof category !== "undefined")
+    professionals.push({ id: newUser.id, category });
 
-  return newUser;
+  return { ...newUser, category };
 }

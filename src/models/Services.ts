c9a7fcc8b7d2c9a95b192437e758c687fd9685ce
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-31 16:52:37 -0500

    add new service and file

--- src/models/Services.ts
+++ src/models/Services.ts
@@ -7,13 +7,32 @@ export let services = [
     category: "Tapicero",
     location: "Carrera 30A # 15 - 20 /B San José",
     description: "Esta es una descripción de prueba",
-    date: "May 17, 2021 03:24:00",
+    date: "2021-05-17T21:40:27.499Z",
     professionals: [
       {
         id: "847be925-ebf3-47ca-937f-192443276f0d",
         name: "Robert Acevedo",
         photo: "https://bit.ly/3uASeyP",
         acceptRequest: true,
+      },
+    ],
+    professional: null,
+  },
+  {
+    id: "258c6646-a774-4bf2-bcd0-65b5bfff344d",
+    client: "f5fba40c-4dc9-44b7-86e1-513d5e7f1bb2",
+    state: 0,
+    title: "Reparacion de cortina",
+    category: "Tapicero",
+    location: "Cra 310 # 12 - 12",
+    description: "Las cortinas de mi sala presentan mucho desgaste en la tela.",
+    date: "2021-05-31T21:40:27.499Z",
+    professionals: [
+      {
+        id: "847be925-ebf3-47ca-937f-192443276f0d",
+        name: "Robert Acevedo",
+        photo: "https://bit.ly/3uASeyP",
+        acceptRequest: false,
       },
     ],
     professional: null,

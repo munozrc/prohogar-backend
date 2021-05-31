9b31fdf923aefd88466e1994cbb9e58b053b79d4
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-31 16:52:21 -0500

    fixed add professional info

--- src/utils/createService.ts
+++ src/utils/createService.ts
@@ -2,15 +2,17 @@ import { professionals } from "../models/Professionals";
 import { v4 as uuidv4 } from "uuid";
 import { ProfessionalModel, ServiceModel } from "../typings";
 import { services } from "../models/Services";
+import { users } from "../models/Users";
 
 export default function createService(service: ServiceModel): ServiceModel {
   const listProfessionals = professionals
     .filter((pro) => pro.category === service.category)
     .map((pro) => {
+      const user = users.find((u) => u.id === pro.id);
       const object: ProfessionalModel = {
         id: pro.id,
-        name: "",
-        photo: "",
+        name: user?.name || "",
+        photo: user?.photo || "",
         acceptRequest: false,
       };
       return object;
